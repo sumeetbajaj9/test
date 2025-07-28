c31a5e3438461a7ce18e34a4cddacb86aa932424
Sumeet Bajaj <sumeetbajaj9@gmail.com>
2025-07-28 15:44:06 +0530

    Update React greeting message

--- react-app/index.jsx
+++ react-app/index.jsx
@@ -1,5 +1,5 @@
 function App() {
-  return <h1>This is developed by Namrata.</h1>;
+  return <h1>This is developed by Tushar.</h1>;
 }
 
 ReactDOM.createRoot(document.getElementById('root')).render(<App />);
